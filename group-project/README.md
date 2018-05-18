63bd37645633d20c892e7e49b1c9ce940975c291
Muatasim Qazi <muatasimqazi@gmail.com>
2018-05-18 03:19:43 -0700

    Updates

---
+++ group-project/README.md
@@ -0,0 +1,1 @@
+# INFO 402 Group Project
---
+++ group-project/src/components/AppBar.js
@@ -0,0 +1,48 @@
+//@ts-check
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Button } from '@material-ui/core';
+
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    flex: {
+        flex: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
+
+function ButtonAppBar(props) {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar style={{ backgroundColor: "#00BCD4" }}>
+                <Toolbar>
+                    <Typography variant="title" color="inherit" className={classes.flex}>
+                        Group Project - INFO 402
+                    </Typography>
+                    <Button color="inherit" href="#problem">Problem</Button>
+                    <Button color="inherit" href="#matters">Why It Matters</Button>
+                    <Button color="inherit" href="#companies">Companies</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+ButtonAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
