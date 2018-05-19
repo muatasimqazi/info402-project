2a9a8f014ba5fedcae9c7494b8b111cc9c77bbde
Muatasim Qazi <muatasimqazi@gmail.com>
2018-05-19 14:07:49 -0700

    fix constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const ROUTES = {
-    main: '/info402-project',
+    main: '/',
     company: '/info402-project/companies/:companyName',
     sources: '/info402-project/sources',
     team: '/info402-project/team',
