3c81e3beb8304366ee128dffe861c21aaff1896a
Somesh Harshavardhan Gopi Krishna <somesh.harsha@yahoo.com>
2018-05-20 20:49:08 -0700

    Edit companies
    Add graphs, conclusion.

--- src/companies.js
+++ src/companies.js
@@ -100,7 +100,7 @@ export const companies = [
         name: "Microsoft",
         image: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Microsoft_Sign_on_German_campus.jpg",
 
-        description: "Being one of the largest tech companies in the world, Microsoft has over 125,000 employees. Of those employees, women make up roughly 25.9% of that number and constitute about 19.0% in tech related fields. As far as leadership goes for Microsoft, women only make up about 19.1% leaders at Microsoft and depicts the existence of gender bias when it comes to leadership roles, promotions, and job reviews.",
+        description: "Being one of the largest tech companies in the world, Microsoft has over 125,000 employees and engages in the provision of developing and marketing software and hardware services. Microsoft has been striving to reduce the existence of a bias across gender and other minorities ",
         link: "/companies/microsoft",
         context: [
           {
@@ -129,20 +129,22 @@ export const companies = [
           }
         ],
         backgroundImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Microsoft_sign_closeup.jpg/1920px-Microsoft_sign_closeup.jpg',
-        trends: ["At Microsoft, despite a positive trend, there is still quite a need to Microsoft to bridge the bias across gender and other minorities. Consequently, the company has created a few employee resource groups as networks for women in technology to develop and support female employees at Microsoft through opportunities such as global conferences, networking events, and mentoring called Women at microsoft. Women at Microsoft also partners with MentorNet, a company that seeks to connect STEM student to mentors in their community. In tandem with renewed hiring policies, Microsoft as a whole, is extensively trying to make inroads to strive against gender biases to make their company a more inclusive."],
+        trends: ["At Microsoft, despite a positive trend, there is still quite a need to bridge the bias across gender and other minorities across their employees; Of this employee pool, women make up roughly only 25.9% of the total and constitute about 19.0% in tech related fields. In leadership at Microsoft, women only make up about 19.1% leaders and this depicts the existence of the bias when it comes to leadership roles, promotions, and job reviews. Consequently, the company has created a few employee resource groups as networks for women in technology to develop and support female employees at Microsoft through opportunities such as global conferences, networking events, and mentoring called Women at Microsoft. Women at Microsoft also partners with MentorNet, a company that seeks to connect STEM student to mentors in their community. In tandem with renewed hiring policies, Microsoft as a whole, is extensively trying to make inroads to strive against gender biases to make their company more inclusive."],
         policies: [
             { "icon": "fal fa-digital-ocean", "name": "DigiGirlz Day", "text": "Designed to introduce girls to the career opportunities available in technology fields" },
-            { "icon": "fal fa-hands", "name": "SitWithMe.org", "text": "to recognize the important role women play in creating future technology" },
-            { "icon": "fal fa-money-bill-alt", "name": "WiRL Leadership Summit Sponsorship", "text": "sponsor 10 women to attendt this summit" }
+            { "icon": "fal fa-hands", "name": "SitWithMe.org", "text": "To recognize the important role women play in creating future technology" },
+            { "icon": "fal fa-money-bill-alt", "name": "WiRL Leadership Summit Sponsorship", "text": "Sponsor 10 women to attend this summit" }
         ],
-        conclusion: "Lorem ipsum dolor sit amet consectetur adipisicing",
+        infograph:"https://photos-5.dropbox.com/t/2/AAAZFKw7JTcIpuwt1BCP7FvpFtUG0B4auyhwpze8F3cAhg/12/591871981/png/32x32/1/_/1/2/ScreenshotMicrosof.png/EKWXreIEGLECIAIoAg/7bqc3ERLbDPT6yGJ0aVRW-KTRkCeyYq1CXrPyRo4Z4s?preserve_transparency=1&size=2048x1536&size_mode=3"
+        infograph:"https://photos-3.dropbox.com/t/2/AAD42HkofANus6Wyh7P951kQxdjlPC5IA9Peb6wjXECIYA/12/591871981/png/32x32/1/_/1/2/ScreenshotMicro.png/EKWXreIEGLECIAIoAg/Yiug4fsCpwjrTS0RNXs25OtJrmr1unBhicLBTX1oYG4?preserve_transparency=1&size=2048x1536&size_mode=3"
+        infograph:"https://photos-4.dropbox.com/t/2/AACQcwB_8G2Ix_2EOp_TaHDlDTk3g3ogmm6ssySYkq53XA/12/591871981/png/32x32/1/_/1/2/ScreenshotMic.png/EKWXreIEGLECIAIoAg/adPQQN8TvM-dUq1uzfFXzzQwOkXcUBho0Q5qEdC07PU?preserve_transparency=1&size=2048x1536&size_mode=3"
+        conclusion: "",
     },
     {
       name: "Deloitte",
       image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/DeloitteToronto4.jpg/2560px-DeloitteToronto4.jpg",
 
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur cupiditate veritatis soluta atque corporis, nam architecto natus culpa porro quisquam animi.",
-      link: "/companies/deloitte",
+      description: "Deloitte is the largest professional services network organization in the world by revenue and number of professionals. Deloitte provides audit, tax, consulting, enterprise risk and financial advisory services with more than 263,900 professionals globally and is a pioneer in tackling gender bias and boasts the best statistics of women contributions across all levels of the firm.",
       context: [
         {
           title: "Company Size",
@@ -166,12 +168,13 @@ export const companies = [
         },
       ],
       backgroundImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/DeloitteToronto3.jpg/2560px-DeloitteToronto3.jpg',
-      trends: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur cupiditateveritatis soluta atque corporis, nam architecto natus culpa porro quisquam animi. Fugit, praesentium minima aut deleniti cupiditate et unde.", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur cupiditateveritatis soluta atque corporis, nam architecto natus culpa porro quisquam animi. Fugit, praesentium minima aut deleniti cupiditate et unde"],
+      trends: ["After researching the extent to which Deloitte’s culture is actually inclusive, the company numbers through the years across all diversity statistics stand out. Deloitte has found a successful way to create and maintain a conducive environment for women and minorities through the years and have the numbers to back it up. Deloitte has been a pioneer with its approach to move away from ERGs and take up inclusion councils which have produced the required results. And the consistency in the importance shown to tackling gender bias makes Deloitte one of the best places to work sans any bias"],
       policies: [
-          { "icon": "fal fa-snowflake", "name": "Policy 1", "text": "Does this policy" },
-          { "icon": "fal fa-child", "name": "Policy 2", "text": "Does this policy" },
-          { "icon": "fal fa-money-bill-alt", "name": "Policy 3", "text": "Does this policy" }
+          { "icon": "fal fa-minus-circle", "name": "Dissolving Workplace Affinity Groups", "text": "This shift away from the traditional was driven by new research findings that highlighted that millennials, who make-up the majority (57%) of Deloitte’s employee base, don’t like being clustered or grouped by demographics." },
+          { "icon": "fal fa-plus-circle", "name": "Inclusion Councils", "text": "Aims to bring more voices into the conversation, cultivate and harness allies and drive change, that bring white men into the conversation" },
+          { "icon": "fal fa-female", "name": "Women in Leadership", "text": "Boasts a vision of increasing women in leadership to at least 30%" }
       ],
-      conclusion: "Lorem ipsum dolor sit amet consectetur adipisicing",
+      infograph: "https://photos-5.dropbox.com/t/2/AACRMmVN-nR6Oak4YRcRX6XrM9b_TCXg1mDhxSLBKwo_hw/12/591871981/png/32x32/1/_/1/2/ScreenshotDel.png/EKWXreIEGLECIAIoAg/NsDROmoDDmsHQRdNEMuztbDMhqpZIuRO0Q4Bsh22-cw?preserve_transparency=1&size=2048x1536&size_mode=3"
+      conclusion: "In the end, as Deloitte rightly points out, these networks and ERGs divide people up into artificial subgroups and isolate them from the networks of power and influence that are such a key part of how leaders identify and promote people. Hence, Deloitte has adopted the task to educate and convince today’s dominant managerial group that inclusion is good for business. Hence, Deloitte makes for one the most conducive work environments without the burden of bias and are leading the response for the outcall to address the gender bias while maintaining their standard of results.",
     },
 ]
