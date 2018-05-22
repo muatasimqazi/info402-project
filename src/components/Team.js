3d2a4bde97fa5b5de1a1cadba95d84bb2e13eb72
Muatasim Qazi <muatasimqazi@gmail.com>
2018-05-22 10:50:55 -0700

    add team contr

--- src/components/Team.js
+++ src/components/Team.js
@@ -9,28 +9,25 @@ const teamMembers = [
     {
         name: "Erica Eaton",
         photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/29511286_889492704565576_1574037852361199471_n.jpg?_nc_cat=0&oh=0608260c11b8322e27674639c8faee29&oe=5B9805D5',
-        contributions: ['a, b, c'],
-        documents: ['a, b, c']
+        contributions: ['research; summarizing and analyzing the research in a paper'],
+        
 
     },
     {
         name: "Luca Vaccarini",
         photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/27867926_10210568876528671_8026972088660661734_n.jpg?_nc_cat=0&oh=80de5e8c9888d5d58983d550995ca64a&oe=5B983B02',
-        contributions: ['a, b, c'],
-        documents: ['a, b, c']
+        contributions: ['researching, building parts of website'],
 
     },
     {
         name: "Muatasim Qazi",
         photo: muatasimqazi,
-        contributions: ['a, b, c'],
-        documents: ['a, b, c']
+        contributions: ['researching Apple and Uber, building parts of website'],
     },
     {
         name: "Somesh Harshavardhan",
         photo: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-1/25182039_1665307006822855_2087445070178243283_o.jpg?_nc_cat=0&oh=79d8f1b02636083af5085dc54b6fbfe3&oe=5B80F186',
-        contributions: ['a, b, c'],
-        documents: ['a, b, c']
+        contributions: ['research, website build'],
     }
 
 ]
@@ -84,22 +81,13 @@ class TeamView extends Component {
                                                         {
                                                             member.contributions.map(cont => {
                                                                 return (
-                                                                    <li key={cont}>cont</li>
+                                                                    <li key={cont}>{cont}</li>
                                                                 );
                                                             })
                                                         }
 
 
-                                                        <Typography variant="subheading" component="h3">
-                                                            Documents
-                                                        </Typography>
-                                                        {
-                                                            member.documents.map(doc => {
-                                                                return (
-                                                                    <li key={doc}>doc</li>
-                                                                );
-                                                            })
-                                                        }
+                                                        
                                                     </Col>
                                                 </Row>
                                             </Card>
